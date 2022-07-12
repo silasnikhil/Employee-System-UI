025701139665cdc625debddf086e33f7ed23f9ad
Nikhil Silas <silasnikhil@gmail.com>
2022-07-12 17:53:26 +0530

    Initial Commit

---
+++ src/components/Employee.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Employee = ({ employee, deleteEmployee }) => {
+
+    const navigate = useNavigate();
+  const editEmployee = (e, id) => {
+    e.preventDefault();
+    navigate(`/editEmployee/${id}`);
+  };
+
+  return (
+    <tr key={employee.id}>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-small text-gray-500">{employee.firstName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-small text-gray-500">{employee.lastName}</div>
+      </td>
+      <td className="text-left px-6 py-4 whitespace-nowrap">
+        <div className="text-small text-gray-500">{employee.emailId}</div>
+      </td>
+      <td className="text-right font-medium text-sm px-6 py-4 whitespace-nowrap">
+        <a
+          onClick={(e, id) => editEmployee(e, employee.id)}
+          className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer px-4"
+        >
+          Edit
+        </a>
+        <a
+          onClick={(e, id) => deleteEmployee(e, employee.id)}
+          className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer"
+        >
+          Delete
+        </a>
+      </td>
+    </tr>
+  );
+};
+
+export default Employee;
